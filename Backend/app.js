19b86afe2535e890302a995f9fd778d3eff2c4b1
Amit Pile <1032202075@mitwpu.edu.in>
2023-07-08 23:31:21 +0530

    deployment changes

--- Backend/app.js
+++ Backend/app.js
@@ -30,18 +30,18 @@ app.use('/api/message/' , messageRoutes);
 
 // ----------------------Deployment----------------------
 
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname1 , '../build')));
-    app.get('*' , (req , res) => {
-        res.sendFile(path.resolve(__dirname1 , 'build' , 'index.html'));
-    });
-}else{
-    app.get('/', (req, res) => {
-        res.send('Server is ready');
-    });
-}
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static(path.join(__dirname1 , '../build')));
+//     app.get('*' , (req , res) => {
+//         res.sendFile(path.resolve(__dirname1 , 'build' , 'index.html'));
+//     });
+// }else{
+//     app.get('/', (req, res) => {
+//         res.send('Server is ready');
+//     });
+// }
 
 
 // ----------------------Deployment----------------------
